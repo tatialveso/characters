f05e602d1fe756fbd25addc76911b0894cb6ffd0
tatialveso <tatiialveso@gmail.com>
2022-11-14 16:03:48 -0300

    :sparkles: set react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CharacterList from './components/CharacterList/CharacterList';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={ <CharacterList /> } />
+      </Routes>
     </div>
   );
 }
